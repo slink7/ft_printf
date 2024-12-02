01513aa6971667c48452e233e2b08b0d7abd9ae3
Sebastien Cambier <slink794@gmail.com>
2024-12-02 22:24:28 +0100

    Added %s conv specifier

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 #include "libft/libft.h"
+#include "src/ft_printf.h"
 
-#define TEST(name, format, ...) printf("\n\n ======= TEST : %s ==================\n\nformat:%s\n", name, format); printf(format, __VA_ARGS__); ft_printf(format, __VA_ARGS__); printf("\n");
+#define TEST(name, format, ...) printf("\n\n ======= TEST : %s ==================\n\nformat:%s\n", name, format); printf("0"format, __VA_ARGS__); ft_printf("1"format, __VA_ARGS__); printf("\n");
 
-#define TESTSS
+#define TESTS
 
 int	main(int argc, char** argv) {
 #ifdef TESTS
@@ -25,6 +26,17 @@ int	main(int argc, char** argv) {
 	TEST("H", "[%-+5d]\t[%-+5d]\t\n", 10, -10);
 
 	TEST("I", "[%01d]\t[%.1d]\t[%06x]\t[%.6x]\n", 456, 456, 142, 142);
+
+	TEST("J", "[%04d]\t[%0 4d]\t[%0+4d]\t[%#4x]\t\n", 5, 5, 5, 5);
+
+	TEST("K", "[%.4d]\t[% .4d]\t[%+.4d]\t[%#.4x]\t\n", 5, 5, 5, 5);
+
+	TEST("L", "[%7.4d]\t[% 7.4d]\t[%+7.4d]\t[%#7.4x]\t\n", 5, 5, 5, 5);
+
+	TEST("M", "[%4s]\t[%4s]\t[%.4s]\t[%.4s]\t\n", "01", "012345", "01", "012345");
+
+	TEST("N", "[%4.4s]\t[%5.4s]\t[%-4.4s]\t[%-8.4s]\t\n", "01", "012345", "01", "012345");
+
 #else
 	ft_printf("%#-0*.*x", 4, 8, 16);
 #endif
--- src/ft_sprintf.c
+++ src/ft_sprintf.c
@@ -6,26 +6,73 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 08:56:32 by scambier          #+#    #+#             */
-/*   Updated: 2024/12/02 09:24:00 by scambier         ###   ########.fr       */
+/*   Updated: 2024/12/02 21:18:58 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
 
+#include "libft.h"
 #include "conversion_specification.h"
+
+void	add_str(t_strbuilder *buffer, t_conv_spec *spec, char *str)
+{
+	if (spec->precision == -1)
+		spec->precision = 0x7FFFFFFF;
+	int	precise_size = ft_min(ft_strlen(str), spec->precision);
+	if (spec->flags & LEFT_ALIGN)
+	{
+		ft_strbuilder_addstr(buffer, str, precise_size);
+		for (int k = 0; k < ft_max(spec->field_width - precise_size, 0); k++)
+			ft_strbuilder_addchar(buffer, ' ');
+	}
+	else
+	{
+		for (int k = 0; k < ft_max(spec->field_width - precise_size, 0); k++)
+			ft_strbuilder_addchar(buffer, ' ');
+		ft_strbuilder_addstr(buffer, str, precise_size);
+	}
+}
+
+static void	handle_specification(t_strbuilder *buffer, t_conv_spec *spec, va_list ap)
+{
+	if (spec->specifier == 's')
+		add_str(buffer, spec, va_arg(ap, char *));
+	(void) spec;
+	(void) ap;
+	//ft_strbuilder_addstr(buffer, "CONVSPEC", 8);
+}
 
 char	*ft_vsprintf(const char	*format, va_list ap)
 {
-	t_conv_spec	spec;
-
-	printf("Success : %d\n", read_conversion_specification(&spec, format, ap));
-	printf("Flags : %d\n", spec.flags);
-	printf("Field_width : %d\n", spec.field_width);
-	printf("Precision : %d\n", spec.precision);
-	printf("Specifier : %c\n", spec.specifier);
-	printf("Length : %d\n", spec.length);
-	return (0);
+	t_conv_spec		spec;
+	t_strbuilder	*buffer;
+	char			*next_spec;
+	char			*out;
+	
+	buffer = ft_strbuilder_new();
+	if (!buffer)
+		return (0);
+	next_spec = ft_strchr(format, '%');
+	while (next_spec)
+	{
+		ft_strbuilder_addstr(buffer, (char *)format, (int)(next_spec - format));
+		if (read_conversion_specification(&spec, next_spec, ap))
+			handle_specification(buffer, &spec, ap);
+		format = next_spec + spec.length;
+		next_spec = ft_strchr(format + 1, '%');
+	}
+	ft_strbuilder_addstr(buffer, (char *)format, ft_strlen(format));
+	out = ft_strbuilder_build(buffer);
+	ft_strbuilder_free(&buffer);
+	// printf("Success : %d\n", read_conversion_specification(&spec, format, ap));
+	// printf("Flags : %d\n", spec.flags);
+	// printf("Field_width : %d\n", spec.field_width);
+	// printf("Precision : %d\n", spec.precision);
+	// printf("Specifier : %c\n", spec.specifier);
+	// printf("Length : %d\n", spec.length);
+	return (out);
 }
 
 char	*ft_sprintf(const char	*format, ...)
