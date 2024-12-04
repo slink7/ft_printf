b5161b41482ccdc6b6cb553000eb90e27d11d085
Sebastien Cambier <slink794@gmail.com>
2024-12-04 20:09:54 +0100

    Fixed some other issues

--- src/add_numbers.c
+++ src/add_numbers.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 19:18:26 by scambier          #+#    #+#             */
-/*   Updated: 2024/12/04 19:58:37 by scambier         ###   ########.fr       */
+/*   Updated: 2024/12/04 20:08:03 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,10 +70,10 @@ void	add_int(t_strbuilder *buffer, t_conv_spec *spec, int value)
 		number_len = ft_max(ft_nbrlen(value, base.size), spec->precision);
 	if (spec->flags & SIGNED || spec->flags & SPACE || value < 0)
 		number_len++;
-	if ((spec->flags & LEFT_ALIGN) && spec->precision < 1 && !value)
-		number_len--;
 	if (spec->precision == -1)
 		spec->precision = 1;
+	if ((spec->flags & LEFT_ALIGN) && spec->precision < 1 && !value)
+		number_len--;
 	if ((spec->flags & SIGNED || spec->flags & SPACE || value < 0) && (spec->flags & ZERO_PAD))
 		ft_strbuilder_addchar(buffer, sign);
 	if (!(spec->flags & LEFT_ALIGN))
@@ -106,6 +106,8 @@ void	add_unsigned(t_strbuilder *buffer, t_conv_spec *spec, unsigned long value, 
 		number_len += ft_strlen(bases[bi].prefix);
 	if (spec->precision == -1)
 		spec->precision = 1;
+	if ((spec->flags & LEFT_ALIGN) && spec->precision < 1 && !value)
+		number_len--;
 	if (spec->flags & ZERO_PAD && spec->flags & PREFIX)
 		ft_strbuilder_addstr(buffer, bases[bi].prefix, ft_strlen(bases[bi].prefix));
 	if (!(spec->flags & LEFT_ALIGN))
