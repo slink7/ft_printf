26726b6d80a991e94971fa188125f89753bda5c5
Sebastien Cambier <slink794@gmail.com>
2024-12-04 01:47:20 +0100

    Fixed prefix & zero padding interaction

--- src/add_numbers.c
+++ src/add_numbers.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 19:18:26 by scambier          #+#    #+#             */
-/*   Updated: 2024/12/03 21:08:03 by scambier         ###   ########.fr       */
+/*   Updated: 2024/12/04 01:46:43 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,34 +79,29 @@ void	add_int_base(t_strbuilder *buffer, unsigned int value, t_base *base, int pr
 
 void	add_unsigned(t_strbuilder *buffer, t_conv_spec *spec, unsigned int value, unsigned int bi)
 {
-	static t_base bases[] = {
+	static t_base	bases[] = {
 		{"0123456789", 10u, ""},
 		{"0123456789abcdef", 16u, "0x"},
 		{"0123456789ABCDEF", 16u, "0X"}
 	};
-	int	number_len;
+	int				number_len;
+	char			padding;
 
+	padding = ' ' + !!(spec->flags & ZERO_PAD) * ('0' - ' ');
 	number_len = ft_max(ft_nbrlen(value, bases[bi].size), spec->precision);
-	if (spec->flags & PREFIX)
+	if (spec->flags & PREFIX && spec->flags & PREFIX)
 		number_len += ft_strlen(bases[bi].prefix);
-
 	if (spec->precision == -1)
 		spec->precision = 1;
-
-	if (spec->flags & LEFT_ALIGN)
-	{
-		if (spec->flags & PREFIX)
-			ft_strbuilder_addstr(buffer, bases[bi].prefix, ft_strlen(bases[bi].prefix));
-		add_int_base(buffer, value, bases + bi, spec->precision);
-	}
-
-	for (int k = 0; k < spec->field_width - number_len; k++)
-		ft_strbuilder_addchar(buffer, (spec->flags & ZERO_PAD) ? '0' : ' ');
-		
+	if (spec->flags & ZERO_PAD && spec->flags & PREFIX)
+		ft_strbuilder_addstr(buffer, bases[bi].prefix, ft_strlen(bases[bi].prefix));
 	if (!(spec->flags & LEFT_ALIGN))
-	{
-		if (spec->flags & PREFIX)
-			ft_strbuilder_addstr(buffer, bases[bi].prefix, ft_strlen(bases[bi].prefix));
-		add_int_base(buffer, value, bases + bi, spec->precision);
-	}
+		for (int k = 0; k < spec->field_width - number_len; k++)
+			ft_strbuilder_addchar(buffer, padding);
+	if ((spec->flags & PREFIX) && !(spec->flags & ZERO_PAD))
+		ft_strbuilder_addstr(buffer, bases[bi].prefix, ft_strlen(bases[bi].prefix));
+	add_int_base(buffer, value, bases + bi, spec->precision);
+	if ((spec->flags & LEFT_ALIGN))
+		for (int k = 0; k < spec->field_width - number_len; k++)
+			ft_strbuilder_addchar(buffer, padding);
 }
